27dc4ef664c0126329c62d5cc85e8d71c2e74256
dzatorski <damian2222221@gmail.com>
2022-09-01 23:45:21 +0200

    Functionality of the application done

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const books = [
 ];
 let title = document.querySelector(`.title-input`);
 let pages = document.querySelector(`.pages-input`);
-const readBtn = document.querySelector(`.read-button`);
+const readBtn = document.querySelector(`.read-button-modal`);
 const addBook = document.querySelector(`.add-book-button`);
 const bookLibrary = document.querySelector(`.book-pages`);
 let authorInp = document.querySelector(`.author-input`);
@@ -18,37 +18,68 @@ let page = document.querySelector(`.page`);
 const modal = document.querySelector(`.modal`);
 const modalButton = document.querySelector(`.open-modal-btn`);
 const closeButton = document.querySelector(`.close`);
+let removeBook = document.querySelectorAll(`.remove-book`);
+const refreshUi = () => {
+  authorInp.value = ``;
+  coverImg.value = ``;
+  title.value = ``;
+  pages.value = ``;
+  readStat = `not read`;
+  readBtn.style.backgroundColor = `red`;
+};
 
+//Opening modal
 modalButton.addEventListener(`click`, () => {
   modal.style.display = `block`;
+  refreshUi();
 });
+//Closing modal with x button
 closeButton.addEventListener(`click`, () => {
   modal.style.display = `none`;
+  refreshUi();
 });
+//Closing modal when clicking outside the modal
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = "none";
+    refreshUi();
   }
 };
+//Creating book objects from the input of the user
 function Book(name, pages, author, status, cover) {
   this.name = name;
   this.pages = pages;
   this.author = author;
   this.status = status;
   this.cover = cover;
-  this.read = function () {
-    if (status === `read`) {
-      status = `not read`;
-    }
-    status = `read`;
-  };
-  this.info = function () {
-    return `${name} is written by ${author}, it has ${pages} pages in total. You ${status} it.`;
-  };
 }
+//Function that adds eventlisteners to buttons and removes specific book from library
+const removeBookFunc = () => {
+  removeBook.forEach((book, index) => {
+    removeBook[index].addEventListener(`click`, () => {
+      console.log(`log`);
+      books.splice(index, 1);
+      render();
+    });
+  });
+};
+
+const readFunc = () => {
+  readBtnAll.forEach((book, index) => {
+    readBtnAll[index].addEventListener(`click`, () => {
+      if (books[index].status === `not read`) {
+        books[index].status = `read`;
+      } else books[index].status = `not read`;
+      render();
+    });
+  });
+};
+//Function responsible for rendering the books in the library and adding functionality to created pages.
 const render = () => {
   bookLibrary.innerHTML = ``;
-  books.forEach((book) => {
+
+  books.forEach((book, index) => {
+    book.index = index;
     const htmlTemplate = `
     <div class="page">
     <span class="remove-book">&times;</span>
@@ -59,31 +90,42 @@ const render = () => {
     <div class="title">Title: ${book.name}</div>
     <div class="author">Author: ${book.author}</div>
     <div class="pages">Pages: ${book.pages}</div>
-    <div class="status">Status ${book.status}</div>
+    <div class="status">Status: ${book.status}</div>
     <div class="read"><button class="read-button">Read</button></div>
     </div>
     `;
-    bookLibrary.insertAdjacentHTML("afterbegin", htmlTemplate);
+    bookLibrary.insertAdjacentHTML("beforeend", htmlTemplate);
+    removeBook = document.querySelectorAll(`.remove-book`);
+    readBtnAll = document.querySelectorAll(`.read-button`);
   });
+  removeBookFunc();
+  readFunc();
 };
 render();
+let readStat = `not read`;
+readBtn.addEventListener(`click`, () => {
+  if (readStat === `not read`) {
+    readStat = `read`;
+    readBtn.style.backgroundColor = `green`;
+    console.log(`green`);
+    render();
+  } else if (readStat === `read`) {
+    readStat = `not read`;
+    readBtn.style.backgroundColor = `red`;
+    console.log(`red`);
+    render();
+  }
+});
+
 addBook.addEventListener(`click`, () => {
   let titleName = title.value;
   let pagesNum = pages.value;
   let authorName = authorInp.value;
   let cover = coverImg.value;
-  let status = `not read`;
+  let status = readStat;
 
   books.push(new Book(titleName, pagesNum, authorName, status, cover));
   render();
-  authorInp.value = ``;
-  coverImg.value = ``;
-  title.value = ``;
-  pages.value = ``;
+  refreshUi();
   modal.style.display = `none`;
-});
-readBtn.addEventListener(`click`, () => {
-  if (readBtn.style.backgroundColor === `red`) {
-    readBtn.style.backgroundColor = `green`;
-  } else readBtn.style.backgroundColor = `red`;
 });
